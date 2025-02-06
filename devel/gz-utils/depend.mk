b3de8396a2b9c75be34ca856951bb1a228dedb4d
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-07 00:32:34 +0100

    [devel/gz-utils] Import gz-utils-3.0.0
    Gazebo Utils, a component of Gazebo, provides general purpose classes and
    functions designed for robotic applications.

---
+++ devel/gz-utils/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	devel/gz-utils
+# Created:			Anthony Mallet on Wed,  5 Feb 2025
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+GZ_UTILS_DEPEND_MK:=		${GZ_UTILS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			gz-utils
+endif
+
+ifeq (+,$(GZ_UTILS_DEPEND_MK)) # -------------------------------------------
+
+GZ_DEPEND_USE+=			gz-utils
+include ../../simulation/gz-sim/depend.common
+
+DEPEND_ABI.gz-utils?=		gz-utils>=2
+DEPEND_DIR.gz-utils?=		../../devel/gz-utils
+
+SYSTEM_SEARCH.gz-utils=\
+  'include/gz/utils[0-9]*/gz/utils.hh'	\
+  'lib/libgz-utils[0-9]*.so'		\
+  $(call gz_system_search,gz-utils, cli log)
+
+endif # GZ_UTILS_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
