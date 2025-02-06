d4e4cfe302d1898df97c516cac0592a33150a28a
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-07 00:35:25 +0100

    [graphics/sdformat] Import sdformat-15.0.0
    SDFormat is an XML file format that describes environments, objects, and robots
    in a manner suitable for robotic applications. SDFormat is capable of
    representing and describing different physic engines, lighting properties,
    terrain, static or dynamic objects, and articulated robots with various
    sensors, and actuators. The format of SDFormat is also described by XML, which
    facilitates updates and allows conversion from previous versions.

---
+++ graphics/sdformat/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	graphics/sdformat
+# Created:			Anthony Mallet on Fri, 7 Feb 2025
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+SDFORMAT_DEPEND_MK:=		${SDFORMAT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			sdformat
+endif
+
+ifeq (+,$(SDFORMAT_DEPEND_MK)) # --------------------------------------------
+
+GZ_DEPEND_USE+=			sdformat
+include ../../simulation/gz-sim/depend.common
+
+DEPEND_ABI.sdformat?=		sdformat>=14
+DEPEND_DIR.sdformat?=		../../graphics/sdformat
+
+SYSTEM_SEARCH.sdformat=\
+  'include/gz/sdformat[0-9]*/sdformat.hh'	\
+  'lib/libsdformat[0-9]*.so'		\
+  $(call gz_system_search,sdformat,)
+
+endif # SDFORMAT_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
