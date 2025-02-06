2dae6427685f5d69a80f9ba9be0257677940d170
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-07 00:29:05 +0100

    [mk/sysdep] Teach robotpkg about protobuf

---
+++ mk/sysdep/protobuf.mk
@@ -0,0 +1,27 @@
+# robotpkg sysdep/protobuf.mk
+# Created:			Anthony Mallet on Mon 19 Nov 2012
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PROTOBUF_DEPEND_MK:=	${PROTOBUF_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		protobuf
+endif
+
+ifeq (+,$(PROTOBUF_DEPEND_MK)) # -------------------------------------------
+
+PREFER.protobuf?=	system
+DEPEND_USE+=		protobuf
+DEPEND_ABI.protobuf?=	protobuf>=2.3.0
+
+SYSTEM_SEARCH.protobuf=	\
+  'bin/protoc:1s/[^0-9.]//gp:% --version:'	\
+  'include/google/protobuf/api.proto'		\
+  'lib/libprotobuf.so'				\
+  'lib/pkgconfig/protobuf.pc:/Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.NetBSD.protobuf=devel/protobuf
+
+endif # PROTOBUF_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
