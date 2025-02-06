581da8e25da94fc328091fa23301e4b475c8a811
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-07 00:37:24 +0100

    [net/gz-transport] Import gz-transport-14.0.0
    Gazebo Transport provides fast and efficient asynchronous message passing,
    services, and data logging.

---
+++ net/gz-transport/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	net/gz-transport
+# Created:			Anthony Mallet on Fri, 7 Feb 2025
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+GZ_TRANSPORT_DEPEND_MK:=	${GZ_TRANSPORT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			gz-transport
+endif
+
+ifeq (+,$(GZ_TRANSPORT_DEPEND_MK)) # ---------------------------------------
+
+GZ_DEPEND_USE+=			gz-transport
+include ../../simulation/gz-sim/depend.common
+
+DEPEND_ABI.gz-transport?=	gz-transport>=13
+DEPEND_DIR.gz-transport?=	../../net/gz-transport
+
+SYSTEM_SEARCH.gz-transport=\
+  'include/gz/transport[0-9]*/gz/transport.hh'	\
+  'lib/libgz-transport[0-9]*.so'		\
+  $(call gz_system_search,gz-transport, log parameters)
+
+endif # GZ_TRANSPORT_DEPEND_MK ---------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
