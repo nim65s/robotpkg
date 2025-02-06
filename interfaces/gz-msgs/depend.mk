10b68d2102bc37275ab1b5b3da59303b7815a708
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-07 00:36:53 +0100

    [interfaces/gz-msgs] Import gz-msgs-11.0.0
    Gazebo messages and functions.

---
+++ interfaces/gz-msgs/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	interfaces/gz-msgs
+# Created:			Anthony Mallet on Fri,  7 Feb 2025
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GZ_MSGS_DEPEND_MK:=	${GZ_MSGS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		gz-msgs
+endif
+
+ifeq (+,$(GZ_MSGS_DEPEND_MK)) # --------------------------------------------
+
+GZ_DEPEND_USE+=		gz-msgs
+include ../../simulation/gz-sim/depend.common
+
+DEPEND_ABI.gz-msgs?=	gz-msgs>=10
+DEPEND_DIR.gz-msgs?=	../../interfaces/gz-msgs
+
+SYSTEM_SEARCH.gz-msgs=\
+  'include/gz/msgs[0-9]*/gz/msgs.hh'			\
+  'lib/libgz-msgs[0-9]*.so'				\
+  '${PYTHON_SYSLIBSEARCH}/gz/msgs[0-9]*/__init__.py'	\
+  $(call gz_system_search, gz-msgs,)
+
+include ../../mk/sysdep/python.mk
+
+endif # GZ_MSGS_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
