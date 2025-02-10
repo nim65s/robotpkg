b690dd503011caae52902163f99967ce5322b331
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-10 15:27:21 +0100

    [graphics/gz-common] Import gz-common-6.0.0
    Gazebo Common, a component of Gazebo, provides a set of libraries that cover
    many different use cases. An audio-visual library supports processing audio and
    video files, a graphics library can load a variety 3D mesh file formats into a
    generic in-memory representation, and the core library of Gazebo Common
    contains functionality that spans Base64 encoding/decoding to thread pools.

---
+++ graphics/gz-common/depend.mk
@@ -0,0 +1,36 @@
+# robotpkg depend.mk for:	graphics/gz-common
+# Created:			Anthony Mallet on Mon, 10 Feb 2025
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+GZ_COMMON_DEPEND_MK:=		${GZ_COMMON_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			gz-common
+endif
+
+ifeq (+,$(GZ_COMMON_DEPEND_MK)) # --------------------------------------------
+
+GZ_DEPEND_USE+=			gz-common
+include ../../simulation/gz-sim/depend.common
+
+DEPEND_ABI.gz-common?=		gz-common>=5
+DEPEND_DIR.gz-common?=		../../graphics/gz-common
+
+SYSTEM_SEARCH.gz-common=\
+  'include/gz/common[0-9]*/gz/common.hh'	\
+  'lib/libgz-common[0-9]*.so'			\
+  $(call gz_system_search,gz-common,		\
+    av events geospatial graphics io profiler)
+
+DEPEND_ABI.libavdevice+=libavdevice>=56.4.100
+
+include ../../mk/sysdep/libavcodec.mk
+include ../../mk/sysdep/libavdevice.mk
+include ../../mk/sysdep/libavformat.mk
+include ../../mk/sysdep/libavutil.mk
+include ../../mk/sysdep/libswscale.mk
+
+endif # GZ_COMMON_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
