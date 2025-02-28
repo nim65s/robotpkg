440b87b33cab29530431d1f103d7ee5443176e16
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-28 16:54:43 +0100

    [simulation/gz-sensors] Import gz-sensors-9.0.0
    Gazebo Sensors, a component of Gazebo, provides numerous sensor models designed
    to generate realistic data from simulation environments. Gazebo Sensors is used
    in conjunction with Gazebo Libraries, and especially relies on the rendering
    capabilities from Gazebo Rendering and physics simulation from Gazebo Physics.

---
+++ simulation/gz-sensors/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	simulation/gz-sensors
+# Created:			Anthony Mallet on Fri, 28 Feb 2025
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+GZ_SENSORS_DEPEND_MK:=		${GZ_SENSORS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			gz-sensors
+endif
+
+ifeq (+,$(GZ_SENSORS_DEPEND_MK)) # -----------------------------------------
+
+GZ_DEPEND_USE+=			gz-sensors
+include ../../simulation/gz-sim/depend.common
+
+DEPEND_ABI.gz-sensors?=		gz-sensors>=8
+DEPEND_DIR.gz-sensors?=		../../simulation/gz-sensors
+
+SYSTEM_SEARCH.gz-sensors=\
+  'include/gz/sensors[0-9]*/gz/sensors.hh'	\
+  'lib/libgz-sensors[0-9]*.so'			\
+  $(call gz_system_search,gz-sensors,)
+
+endif # GZ_SENSORS_DEPEND_MK -----------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
