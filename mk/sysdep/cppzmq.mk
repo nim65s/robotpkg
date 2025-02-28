0bdd6b395f4758c198a7721c90396a0ff3ce5bfd
Anthony Mallet <anthony.mallet@laas.fr>
2025-02-28 16:53:15 +0100

    [mk/sysdep] Fix cppzmp search for debian<12 or ubuntu<24
    cppzmq is not really packaged on debian<12 and ubuntu<24 but rather partially
    included in libzmq. Make some files optional in SYSTEM_SEARCH in order to
    support this.
    
    While here, add package hints.

--- mk/sysdep/cppzmq.mk
+++ mk/sysdep/cppzmq.mk
@@ -17,10 +17,18 @@ DEPEND_USE+=		cppzmq
 
 DEPEND_ABI.cppzmq?=	cppzmq
 
+# ubuntu<24 and debian<12 bundle the header inside libzmq-dev, so mark the
+# cmake and pkg-config file as optional. If found, they will still serve
+# setting the cmake and pkg-config paths correctly.
+#
 SYSTEM_SEARCH.cppzmq=\
   'include/zmq.hpp'				\
-  'share/cmake/cppzmq/cppzmqConfig.cmake'	\
-  'lib/pkgconfig/cppzmq.pc:/Version/s/[^0-9.]//gp'
+  'share/cmake/cppzmq/cppzmqConfig.cmake:::~'	\
+  'lib/pkgconfig/cppzmq.pc:/Version/s/[^0-9.]//gp::~'
+
+SYSTEM_PKG.Debian.cppzmq=	cppzmq-dev
+SYSTEM_PKG.NetBSD.cppzmq=	net/cppzmq
+SYSTEM_PKG.RedHat.cppzmq=	cppzmq-devel
 
 endif # CPPZMQ_DEPEND_MK ---------------------------------------------------
 
