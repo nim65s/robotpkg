0f3dce848a321419f3de2d1fdf9d99abc2e1ea4c
Anthony Mallet <anthony.mallet@laas.fr>
2025-03-24 18:26:41 +0100

    [mk/sysdep] Teach robotpkg about m4
    If GNU m4 is required, there is sysdep/gm4.mk. Modify the latter to first
    search for any m4, and then override M4 environment variable.

--- mk/sysdep/gm4.mk
+++ mk/sysdep/gm4.mk
@@ -1,4 +1,4 @@
-# robotpkg sysdep/autoconf.mk
+# robotpkg sysdep/gm4.mk
 # Created:			Anthony Mallet on Mon Feb  5 2018
 #
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -10,15 +10,18 @@ endif
 
 ifeq (+,$(GM4_DEPEND_MK)) # ------------------------------------------------
 
+# search regular m4 first so that export M4 below can overrides it
+include ../../mk/sysdep/m4.mk
+
 PREFER.gm4?=		system
 
 DEPEND_USE+=		gm4
 DEPEND_ABI.gm4?=	gm4>=1.4
-DEPEND_METHOD.gm4?=build
+DEPEND_METHOD.gm4?=	build
 SYSTEM_SEARCH.gm4=\
   'bin/{g,gnu,}m4:/^[mM]4/{/GNU/!s/.*/0/;s/.* //;p}:% --version'
 
-export M4=	$(word 1,${SYSTEM_FILES.gm4})
+export M4=		$(word 1,${SYSTEM_FILES.gm4})
 
 SYSTEM_DESCR.gm4=	$(subst gm4,GNU m4,${DEPEND_ABI.gm4})
 SYSTEM_PKG.Ubuntu.gm4=	m4
