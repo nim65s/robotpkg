cee1ceb36d7680db0d40214fe77ff07d0e941bc3
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-02 20:18:42 +0100

    [mk/sysdep/swig.mk] add SYSTEM_PKG.Debian

--- mk/sysdep/swig.mk
+++ mk/sysdep/swig.mk
@@ -20,6 +20,7 @@ DEPEND_METHOD.swig+=	build
 SYSTEM_SEARCH.swig=\
   'bin/swig{[0-9]*,}:/Version/{s/[^.0-9]//gp;q;}:% -version'
 
+SYSTEM_PKG.Debian.swig=	swig
 SYSTEM_PKG.Gentoo.swig=	dev-lang/swig
 
 export SWIG=		$(word 1,${SYSTEM_FILES.swig})
