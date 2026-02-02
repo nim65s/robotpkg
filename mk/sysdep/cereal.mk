d223d870b320f55e4dfe962f0e9e2e73c61cb453
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-02 20:18:29 +0100

    [mk/sysdep/cereal.mk] Init

---
+++ mk/sysdep/cereal.mk
@@ -0,0 +1,29 @@
+# robotpkg			sysdep/cereal.mk
+# Created:			Guilhem Saurel on Mon, 27 Oct 2025
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+CEREAL_DEPEND_MK:=		${CEREAL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			cereal
+endif
+
+ifeq (+,$(CEREAL_DEPEND_MK)) # --------------------------------------
+
+PREFER.cereal?=			system
+
+DEPEND_USE+=			cereal
+
+DEPEND_METHOD.cereal?=		build
+DEPEND_ABI.cereal?=		cereal>=1
+
+SYSTEM_SEARCH.cereal=		\
+  'include/cereal/cereal.hpp'	\
+  'lib/{x86_64-linux-gnu/,}cmake/cereal/cerealConfigVersion.cmake:/set.PACKAGE_VERSION/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Debian.cereal=	libcereal-dev
+
+endif # CEREAL_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
