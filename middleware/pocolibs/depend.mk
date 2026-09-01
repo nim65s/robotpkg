43cfa05b84a936b25799b85fa9ae8ec385734581
Anthony Mallet <anthony.mallet@laas.fr>
2026-09-01 18:14:23 +0200

    [middleware/pocolibs] Update to 3.4.0
    Changes since 3.3:
    * Make pocolibs compatible with C23.
    * Removes the old taskSpawn() API, replaced by taskSpawn2().
    
    Require >=3.4 by default
    While here, update depend.mk to check the installed version from the .pc file

--- middleware/pocolibs/depend.mk
+++ middleware/pocolibs/depend.mk
@@ -9,18 +9,21 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		pocolibs
 endif
 
-ifeq (+,$(POCOLIBS_DEPEND_MK))
+ifeq (+,$(POCOLIBS_DEPEND_MK)) # -------------------------------------------
+
 PREFER.pocolibs?=	robotpkg
 
 DEPEND_USE+=		pocolibs
 
-DEPEND_ABI.pocolibs?=	pocolibs>=3.1
+DEPEND_ABI.pocolibs?=	pocolibs>=3.4
 DEPEND_DIR.pocolibs?=	../../middleware/pocolibs
 
 SYSTEM_SEARCH.pocolibs=\
-	bin/h2		\
-	include/commonStructLib.h	\
-	lib/pkgconfig/pocolibs.pc
-endif
+  'bin/h2'			\
+  'include/commonStructLib.h'	\
+  'lib/libportLib.so'		\
+  'lib/pkgconfig/pocolibs.pc:/Version/s/[^.0-9]//gp'
+
+endif # POCOLIBS_DEPEND_MK -------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
