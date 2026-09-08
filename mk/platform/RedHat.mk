2f53255ee924de017d173885e4d66b36871a6099
Anthony Mallet <anthony.mallet@laas.fr>
2026-09-08 11:55:39 +0200

    [mk/platform] Fix longstanding issue with rpmbuild variables on redhat
    System packages built with rpmbuild have package-note flags in their linker
    LDFLAGS, which can too easily leak from installed packages as a spurious
    -specs=/usr/lib/rpm/redhat/redhat-package-notes (typically via Qt and Tcl
    config files, but also through many others). These flags reference RPM_ARCH
    and other RPM variables, and when the build is executed outside an rpmbuild
    context, this obviously fails.
    The required variables are therefore provided here with ad-hoc values, so that
    it's not necessary to manually fix the system packages.

--- mk/platform/RedHat.mk
+++ mk/platform/RedHat.mk
@@ -8,4 +8,16 @@ ifeq (${MACHINE_ARCH},x86_64)	# 64bits arch
   SYSLIBDIR?=	lib64 lib
 endif
 
+# system packages built with rpmbuild have package-note flags in their linker
+# LDFLAGS, which can too easily leak from installed packages as a spurious
+# -specs=/usr/lib/rpm/redhat/redhat-package-notes (typically via Qt and Tcl
+# config files, but also through many others). These flags reference RPM_ARCH
+# and other RPM variables, and when the build is executed outside an rpmbuild
+# context, this obviously fails.
+# The required variables are therefore provided here with ad-hoc values:
+export RPM_ARCH=		${MACHINE_ARCH}
+export RPM_PACKAGE_RELEASE=	${PKGREVISION}
+export RPM_PACKAGE_VERSION=	${PKGREVISION_NOREV}
+export RPM_PACKAGE_NAME=	robotpkg-${PKGBASE}
+
 include ${ROBOTPKG_DIR}/mk/platform/Linux.mk
