43621d88c9b3e7ca1b04df7429530885f16eb512
Anthony Mallet <anthony.mallet@laas.fr>
2026-09-08 15:45:24 +0200

    [hardware/py-odri-control-interface] Fix build with cmake>=4 and g++-16
    * Remove patch-aa, eigen3 in robotpkg now always have cmake config files
    * Add missing <cassert> for assert(3)
    * Force cmake minimum version to 3.5

---
+++ hardware/py-odri-control-interface/patches/patch-src_utils.cpp
@@ -0,0 +1,12 @@
+For assert(3)
+
+--- src/utils.cpp~	2024-05-04 10:44:12.000000000 +0200
++++ src/utils.cpp	2026-09-08 15:43:27.112987950 +0200
+@@ -8,6 +8,7 @@
+  *
+  * @brief Different utils for using the framework.
+  */
++#include <cassert>
+ 
+ #include <yaml-cpp/yaml.h>
+ 
